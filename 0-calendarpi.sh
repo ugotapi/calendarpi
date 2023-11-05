af3c5e41f02f821ec1c72b07a1a41f664437f2b3
Ugotapi <rudischmitz@gmail.com>
2023-11-05 17:48:41 -0600

    0-calendarpi.sh

--- 0-calendarpi.sh
+++ 0-calendarpi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/rudischmitz71/blahblah/main/1-calendarpi.sh
-wget https://raw.githubusercontent.com/rudischmitz71/blahblah/main/4-finalize-calendarpi.sh
+wget hhttps://raw.githubusercontent.com/ugotapi/calendarpi/main/1-calendarpi.sh
+wget https://raw.githubusercontent.com/ugotapi/calendarpi/main/4-finalize-calendarpi.sh
 cp /home/$USER/4-finalize-calendarpi.sh /home/$USER/Desktop/4-finalize-calendarpi.sh
 sudo chmod +x /home/$USER/*.sh
 /home/$USER/1-calendarpi.sh
