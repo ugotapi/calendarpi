696db8e8301887b1360ce46f7a2a8ce22ba5de47
Ugotapi <rudischmitz@gmail.com>
2023-11-05 17:02:41 -0600

    Create 4-finalize-calendarpi.sh

---
+++ 4-finalize-calendarpi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#install wp-cli wordpress cli and some modules
+cd ~
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+sudo chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
+
+cd /var/www/html
+sudo wp plugin install --activate "fullwidth-templates" --allow-root
+sudo wp plugin install --activate "ics-calendar" --allow-root
+
+#fix wordpress folder permissions
+sudo chown www-data:www-data /var/www/html/wp-content/upgrade/
+
+cd ~
+# refresh every 15 minutes
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "*/15 * * * * DISPLAY=:0 /home/$USER/refresh.sh" >> mycron
+#install new cron file
+crontab mycron
+rm mycron
