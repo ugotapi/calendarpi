2e2e3a4739dace1a806faf321711e37e72509da1
Ugotapi <rudischmitz@gmail.com>
2023-11-26 16:48:56 -0600

    Update 0-calendarpi.sh

--- 0-calendarpi.sh
+++ 0-calendarpi.sh
@@ -5,11 +5,12 @@ wget https://raw.githubusercontent.com/ugotapi/calendarpi/main/nextsunday.txt
 wget https://raw.githubusercontent.com/ugotapi/calendarpi/main/thissunday.txt
 wget https://raw.githubusercontent.com/ugotapi/calendarpi/main/twosundaysfromnow.txt
 wget https://raw.githubusercontent.com/ugotapi/calendarpi/main/month.txt
+
 cp /home/$USER/nextsunday.txt /home/$USER/Desktop/nextsunday.txt 
-cp /home/$USER/lastsunday.txt /home/$USER/Desktop/thissunday.txt
-cp /home/$USER/lastsunday.txt /home/$USER/Desktop/thissunday.txt
+cp /home/$USER/thissunday.txt /home/$USER/Desktop/thissunday.txt
 cp /home/$USER/twosundaysfromnow.txt /home/$USER/Desktop/twosundaysfromnow.txt
 cp /home/$USER/month.txt /home/$USER/Desktop/month.txt
 cp /home/$USER/4-finalize-calendarpi.sh /home/$USER/Desktop/4-finalize-calendarpi.sh
+
 sudo chmod +x /home/$USER/*.sh
 /home/$USER/1-calendarpi.sh
